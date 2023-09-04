b4a5ed7efbbca421475ddac6ac75e5d29a75b202
valed-dm <dmvaled@gmail.com>
2023-09-04 08:21:09 +0300

    get_merged_intervals allocated separately

--- utils/task3/prepare_data.py
+++ utils/task3/prepare_data.py
@@ -2,22 +2,7 @@
 
 from dataclasses import dataclass, field
 
-from utils.task3 import merge_intervals
-
-
-def get_merged_intervals(start, finish):
-    """Merges overlapping intervals"""
-
-    intervals_to_merge = list(map(list, zip(start, finish)))
-    merged = merge_intervals(intervals_to_merge)
-    unpacked = [j for i in merged for j in i]
-    merged_start = unpacked[::2]
-    merged_finish = unpacked[1::2]
-
-    print(merged_start)
-    print(merged_finish)
-
-    return merged_start, merged_finish
+from utils.task3.get_merged_intervals import get_merged_intervals
 
 
 @dataclass
